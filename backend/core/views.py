4aff61580f44b8104c96ca60c36d80e3f078d7c2
justinWGU <Justin_ortiz123@yahoo.com>
2025-04-23 14:20:38 -0700

    Adds view to retrieve scores from match

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import MatchSerializer
+from rest_framework import status
+from .models import Match
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def get_scores(request):
+    match = Match.objects.get(id=1)
+    serializer = MatchSerializer(match)
+    data = serializer.data
+    return Response(data=data, status=status.HTTP_200_OK)
