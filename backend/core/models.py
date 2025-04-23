dec9250d424e543b3268197fabe3bb63f3141c14
justinWGU <Justin_ortiz123@yahoo.com>
2025-04-23 12:52:19 -0700

    Adds Match model and applies migrations

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Match(models.Model):
+    leftTeamScore = models.IntegerField(null=True, blank=True)
+    rightTeamScore = models.IntegerField(null=True, blank=True)
