ac1e5b63a9f5f087228041a347d3e48f9f669bd5
justinWGU <Justin_ortiz123@yahoo.com>
2025-04-28 12:40:23 -0700

    Adds basic model tests
    Creates separate testing directory

--- backend/core/tests/test_models.py
+++ backend/core/tests/test_models.py
@@ -2,11 +2,17 @@ from django.test import TestCase
 from core.models import Match
 
 # Create your tests here.
-
+# test --parallel.
+# test - -keepdb
 class MatchTestCase(TestCase):
     def setUp(self):
         Match.objects.create(id=0, leftTeamScore=0, rightTeamScore=0)
 
-    def test_match_creation(self):
+    def test_match_object_creation(self): # what we're testing, what the result should be
         match = Match.objects.get(id=0)
         self.assertEqual(match.leftTeamScore, 0, "Left team score should be a number 0.")
+        self.assertEqual(match.rightTeamScore, 0, "Right team score should be a number 0.")
+
+    def test_object_not_found(self):
+        with self.assertRaises(Match.DoesNotExist):
+            match = Match.objects.get(id=1)
