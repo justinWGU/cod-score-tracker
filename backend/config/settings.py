009cdd8598d4161d1754e01c9e04faaed02e0b27
justinWGU <Justin_ortiz123@yahoo.com>
2025-04-28 12:39:29 -0700

    Adds cors headers support for frontend api connections

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleWare",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
