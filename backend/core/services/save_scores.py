be171c7a3fcbe4f2ed19645ba6ee965c8a6b3348
justinWGU <Justin_ortiz123@yahoo.com>
2025-05-03 18:23:19 -0700

    Add type hinting to scores param

--- backend/core/services/save_scores.py
+++ backend/core/services/save_scores.py
@@ -1,7 +1,7 @@
 from core.models import Match
 
 
-def save_scores(match_id, scores):
+def save_scores(match_id, scores: dict):
     """ Saves updated match scores to DB."""
 
     print("Saving scores...")
@@ -9,16 +9,9 @@ def save_scores(match_id, scores):
     scores_left = scores.get('teamLeft')
     scores_right = scores.get('teamRight')
 
-    try:
-        match = Match.objects.get(id=match_id)
-        match.leftTeamScore = scores_left
-        match.rightTeamScore = scores_right
-        match.save()
-    except Match.DoesNotExist as dne:
-        print(f"No match with id {match_id} found.")
-        raise dne
-    except Exception as e:
-        print("Unknown error occurred while attempting to save match to DB.")
-        raise e
+    match = Match.objects.get(id=match_id)
+    match.leftTeamScore = scores_left
+    match.rightTeamScore = scores_right
+    match.save()
 
     print("Scores updated...")
