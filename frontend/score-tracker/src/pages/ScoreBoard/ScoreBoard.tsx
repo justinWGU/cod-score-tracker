79d5517b43cd0c4ec627d3c088010cda011c1010
justinWGU <Justin_ortiz123@yahoo.com>
2025-05-09 14:03:37 -0700

    Adds state and rendering logic to display a message if an error occurs during fetch.
    Fixes bug in useEffect that was not catching rejected promise from fetch.

--- frontend/score-tracker/src/pages/ScoreBoard/ScoreBoard.tsx
+++ frontend/score-tracker/src/pages/ScoreBoard/ScoreBoard.tsx
@@ -5,6 +5,7 @@ import TeamPanel from './TeamPanel/TeamPanel.tsx';
 import GameDetails from './GameDetails/GameDetails.tsx';
 
 function ScoreBoard() {
+  const [hasError, setHasError] = useState<boolean>(false);
   const [scores, setScores] = useState<Scores>({left: 150, right: 200});
   const [teams, setTeams] = useState<Teams>({left: 'Optic Gaming', right: 'Faze Clan'});
   const [seriesWins, setSeriesWins] = useState<SeriesWins>({left: 2, right: 1});
@@ -13,25 +14,33 @@ function ScoreBoard() {
 
   // continuously fetch scores every 10 secs
   useEffect( () => {
-    try {
-      const interval = setInterval( async () => {
+    const interval = setInterval( async () => {
+      try {
         const data: ScoresAPIData = await getScores(1);
         setScores({left: data.leftTeamScore, right: data.rightTeamScore});
         // more state will be set here
-      }, 100000);
-
-      return () => clearInterval(interval);
-    } catch (err) {
-      console.error(err);
-    }
+      } catch (err) {
+          setHasError(true);
+          console.error(err);
+      }
+    }, 5000);
+    return () => clearInterval(interval);
   }, []);
   
-  return(
-    <div className='bg-white shadow-2xl rounded-2xl p-5 w-xl min-h-40 flex gap-2 justify-center items-center border-2 border-gray-500'>
-      <TeamPanel team={teams.left} score={scores.left}/>
-      <GameDetails winsLeft={seriesWins.left} winsRight={seriesWins.right} mode={gameDetails.mode}/>
-      <TeamPanel team={teams.right} score={scores.right}/>
-    </div>
-  );
+  if (hasError) {
+    return (
+      <div className='bg-white shadow-2xl rounded-2xl p-5 w-xl min-h-40 flex gap-2 justify-center items-center border-2 border-gray-500'>
+        <p className='text-red-600'>Error occurred fetching scores!</p>
+      </div>
+    );
+  } else {
+    return(
+      <div className='bg-white shadow-2xl rounded-2xl p-5 w-xl min-h-40 flex gap-2 justify-center items-center border-2 border-gray-500'>
+        <TeamPanel team={teams.left} score={scores.left}/>
+        <GameDetails winsLeft={seriesWins.left} winsRight={seriesWins.right} mode={gameDetails.mode}/>
+        <TeamPanel team={teams.right} score={scores.right}/>
+      </div>
+    );
+  }
 }
 export default ScoreBoard;
