dececf877dc5ab20be066e702e0ad1069e50d052
justinWGU <Justin_ortiz123@yahoo.com>
2025-05-09 13:54:57 -0700

    Adds ErrorBoundary around ScoreBoard component

--- frontend/score-tracker/src/App.tsx
+++ frontend/score-tracker/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 import ScoreBoard from './pages/ScoreBoard/ScoreBoard';
+import { ErrorBoundary } from 'react-error-boundary';
+
 
 function App() {
   return (
     <div className='flex justify-center items-center bg-gray-100'>
-      <ScoreBoard />
+      <ErrorBoundary fallback={<div>ScoreBoard Fallback</div>}>
+        <ScoreBoard />
+      </ErrorBoundary>
     </div>
   );
 }
