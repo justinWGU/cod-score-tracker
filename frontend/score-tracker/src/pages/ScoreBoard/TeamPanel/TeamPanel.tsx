691309f8dc1c5f927bb65caafaafbcebb9f13c4e
justinWGU <Justin_ortiz123@yahoo.com>
2025-05-09 17:41:45 -0700

    Adjusts stying to round borders and increase padding
    Refactors h1 elements to p tags

--- frontend/score-tracker/src/pages/ScoreBoard/TeamPanel/TeamPanel.tsx
+++ frontend/score-tracker/src/pages/ScoreBoard/TeamPanel/TeamPanel.tsx
@@ -5,9 +5,9 @@ interface TeamPanelProps {
 
 function TeamPanel({ team, score }: TeamPanelProps) {
   return (
-   <div className="border-2 border-gray-600 flex gap-1 p-5">
-    {<div className=''>{team}</div>}
-    {score !== null ? <h1 className=''>{score}</h1> : <h1 className=''>--</h1>}
+   <div className="rounded-2xl items-center border-2 border-gray-600 flex gap-1 p-5">
+    <p>{team}</p>
+    {score !== null ? <p className="p-3">{score}</p> : <p className="p-3">--</p>}
    </div>
   ); 
  } export default TeamPanel;
