d4fd785631fc7ced64305752afa25318b649304a
justinWGU <Justin_ortiz123@yahoo.com>
2025-05-09 17:42:43 -0700

    Adds unit tests to test it renders properly given different data

--- frontend/score-tracker/src/pages/ScoreBoard/TeamPanel/__tests__/TeamPanel.test.tsx
+++ frontend/score-tracker/src/pages/ScoreBoard/TeamPanel/__tests__/TeamPanel.test.tsx
@@ -1,7 +1,32 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import TeamPanel from '../TeamPanel';
-import { test } from 'vitest';
+import { expect, test } from 'vitest';
 
 test('renders without error', () => {
-  render(<TeamPanel team='Optic Gaming' score={200}/>);
+  render(<TeamPanel team='Optic Texas' score={200}/>);
+});
+
+test('renders score when passed a negative number', () => {
+  render(<TeamPanel team='Atlanta Faze' score={-300}/>);
+  const teamElement: HTMLElement = screen.getByText('Atlanta Faze');
+  const negScoreElement: HTMLElement = screen.getByText(-300);
+
+  expect(teamElement).toBeVisible();
+  expect(negScoreElement).toBeVisible();
+});
+
+test('renders score when passed a value of 0', () => {
+  render(<TeamPanel team='Atlanta Faze' score={0}/>);
+  const teamElement: HTMLElement = screen.getByText('Atlanta Faze');
+  const zeroScoreElement: HTMLElement = screen.getByText(0);
+
+  expect(teamElement).toBeVisible();
+  expect(zeroScoreElement).toBeVisible();
+});
+
+test('renders for the team scores when score prop is null', () => {
+  render(<TeamPanel team='Vegas Faclons' score={null}/>);
+  const scoreElement: HTMLElement = screen.getByText('--');
+
+  expect(scoreElement).toBeVisible();
 });
