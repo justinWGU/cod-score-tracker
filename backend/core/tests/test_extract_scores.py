7f2d7b0b0b17711b53fae38e362d1a26d2287439
justinWGU <Justin_ortiz123@yahoo.com>
2025-05-11 17:59:40 -0700

    Adds test for refactored function

--- backend/core/tests/test_extract_scores.py
+++ backend/core/tests/test_extract_scores.py
@@ -15,23 +15,28 @@ class TestExtractScores(TestCase):
         load_dotenv()
 
 
-    @patch('core.services.extract_scores.genai.Client')
-    def test_extract_scores(self, mock_client_class):
+    # @patch('core.services.extract_scores.genai.Client')
+    @patch('core.services.extract_scores.get_response')
+    def test_extract_scores(self, mock_get_response):
         """Assert extract_scores calls correct functions and returns dictionary."""
 
-        # mock Client class to return a MagicMock instance
-        mock_client = mock_client_class.return_value
+        # # mock Client class to return a MagicMock instance
+        # mock_client = mock_client_class.return_value
+        #
+        # # mock files.upload() to return a file obj
+        # mock_file = MagicMock()
+        # mock_client.files.upload.return_value = mock_file
+        # mock_file.name = 'mock_file'
+        #
+        # # mock generate_content to return a response obj
+        # mock_response = MagicMock()
+        # mock_client.models.generate_content.return_value = mock_response # generate_content returns an obj
+        # mock_response.text = "{'team_left': 23, 'team_right': 42}" # assign directly to text since it's an attr
+        # mock_client.files.delete.return_value = None
 
-        # mock files.upload() to return a file obj
-        mock_file = MagicMock()
-        mock_client.files.upload.return_value = mock_file
-        mock_file.name = 'mock_file'
-
-        # mock generate_content to return a response obj
         mock_response = MagicMock()
-        mock_client.models.generate_content.return_value = mock_response # generate_content returns an obj
-        mock_response.text = "{'team_left': 23, 'team_right': 42}" # assign directly to text since it's an attr
-        mock_client.files.delete.return_value = None
+        mock_get_response.return_value = mock_response
+        mock_response.text =  "{'team_left': 23, 'team_right': 42}"
 
         # call actual method
         scores = extract_scores()
@@ -40,6 +45,7 @@ class TestExtractScores(TestCase):
         self.assertTrue(isinstance(scores, dict))
         self.assertEqual(scores, {'team_left': 23, 'team_right': 42})
 
+    # TODO: create test for get_response
 
     @patch('core.services.extract_scores.extract_scores_test')
     def test_extract_scores_with_test_equals_true(self, mock_extract_scores_test):
