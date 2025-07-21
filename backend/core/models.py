8734744ad6d30b8de6d8d1bcf4051916891d0152
justinWGU <Justin_ortiz123@yahoo.com>
2025-07-21 10:59:45 -0700

    Updates models

--- backend/core/models.py
+++ backend/core/models.py
@@ -4,5 +4,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Match(models.Model):
+    scheduled_time = models.DateTimeField(null=True, blank=True)
+    right_team = models.CharField(blank=True)
     leftTeamScore = models.IntegerField(null=True, blank=True)
     rightTeamScore = models.IntegerField(null=True, blank=True)
+    map_set = models
+
+    # TODO: Create a new dev branch from main. Figure out how to properly create models
+class Map(models.Model):
+    map = models.CharField(blank=True)
+    mode = models
