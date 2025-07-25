ece7c8268c2a98730982a76f0fe8c6723e43fcad
justinWGU <Justin_ortiz123@yahoo.com>
2025-07-24 18:54:04 -0700

    Adds website url to csrf treusted origins

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,6 +13,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
+CSRF_TRUSTED_ORIGINS = ['https://codscoretracker.pythonanywhere.com/']
+
 ALLOWED_HOSTS = [
     'localhost:8000', 
     'localhost',
