0e0f8daefb853f39f67e2fa564ca23cf236cc55b
justinWGU <Justin_ortiz123@yahoo.com>
2025-09-02 16:14:38 -0700

    Fixes error in GameDetails test file

--- frontend/score-tracker/src/pages/ScoreBoard/GameDetails/__tests__/GameDetails.test.tsx
+++ frontend/score-tracker/src/pages/ScoreBoard/GameDetails/__tests__/GameDetails.test.tsx
@@ -3,13 +3,15 @@ import GameDetails from '../GameDetails';
 import { expect, test } from 'vitest';
 
 test('renders without error', () => {
-  render(<GameDetails winsLeft={50} winsRight={19} mode='Search and Destroy'/>);
+  render(<GameDetails winsLeft={50} winsRight={19} mode='Search and Destroy' map='Protocol'/>);
 
   const winsLeft = screen.getByText('50');
   const winsRight = screen.getByText('19');
   const mode = screen.getByText('Search and Destroy');
+  const map = screen.getByText('Protocol');
 
   expect(winsLeft).toBeInTheDocument();
   expect(winsRight).toBeInTheDocument();
   expect(mode).toBeInTheDocument();
+  expect(map).toBeInTheDocument();
 });
