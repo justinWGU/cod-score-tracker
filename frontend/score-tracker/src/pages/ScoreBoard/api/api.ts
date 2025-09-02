ac63267ceb5efc2f9eee34e864f0e2a8a94f233f
justinWGU <Justin_ortiz123@yahoo.com>
2025-09-02 16:07:48 -0700

    Changes url to point to render url

--- frontend/score-tracker/src/pages/ScoreBoard/api/api.ts
+++ frontend/score-tracker/src/pages/ScoreBoard/api/api.ts
@@ -1,7 +1,8 @@
 export const getScores = async (id: number) => {
   console.log("Fetching...");
-  // const url: string = 'https://codscoretracker.pythonanywhere.com/api/get-scores/';
-  const url: string = 'http://localhost:8000/api/get-scores/';
+  // const url = 'https://codscoretracker.pythonanywhere.com/api/get-scores/';
+  // const url = 'http://localhost:8000/api/get-scores/';
+  const url = 'https://cod-score-tracker.onrender.com/api/get-scores/';
   
   const response = await fetch(url + `?id=${id}`);
   
