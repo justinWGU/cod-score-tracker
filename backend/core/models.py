9ad9afbe5b76645fa54fafacaaa64ac28db0b9fa
justinWGU <Justin_ortiz123@yahoo.com>
2025-09-18 12:00:46 -0700

    Creates one-to-many relationship between Match and Map respectively

--- backend/core/models.py
+++ backend/core/models.py
@@ -2,15 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
 class Match(models.Model):
     scheduled_time = models.DateTimeField(null=True, blank=True)
     right_team = models.CharField(blank=True)
+    left_team = models.CharField(blank=True)
     leftTeamScore = models.IntegerField(null=True, blank=True)
     rightTeamScore = models.IntegerField(null=True, blank=True)
-    map_set = models
 
-    # TODO: Create a new dev branch from main. Figure out how to properly create models
 class Map(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE, blank=True, null=True)
     map = models.CharField(blank=True)
-    mode = models
+    mode = models.CharField(blank=True)
