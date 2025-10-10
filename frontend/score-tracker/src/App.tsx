8f4f13463626c26ce62caee04734dc72c82b8705
justinWGU <Justin_ortiz123@yahoo.com>
2025-10-09 20:02:14 -0700

    Lifts BrowserRouter to main.tsx

--- frontend/score-tracker/src/App.tsx
+++ frontend/score-tracker/src/App.tsx
@@ -2,16 +2,14 @@ import './App.css'
 import Landing from './pages/Landing/Landing';
 import ScoreBoard from './pages/ScoreBoard/ScoreBoard';
 import { ErrorBoundary } from 'react-error-boundary';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={ <Landing /> }/>
-        <Route path='/scoreboard' element={ <ErrorBoundary fallback={<div>ScoreBoard Fallback</div>}><ScoreBoard /></ErrorBoundary> } />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path='/' element={ <Landing /> }/>
+      <Route path='/scoreboard' element={ <ErrorBoundary fallback={<div>ScoreBoard Fallback</div>}><ScoreBoard /></ErrorBoundary> } />
+    </Routes>
   );
 }
 
